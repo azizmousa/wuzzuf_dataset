bc5a476aa21af18c425ad08e7d1c52ec4ee1efcf
azizmousa <azizmousa1010@gmail.com>
2021-07-02 20:52:17 +0200

    implement method to remove the null rows

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/preprocess/DataCleaner.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/preprocess/DataCleaner.java
@@ -2,28 +2,29 @@ package com.aziz.wuzzuf.preprocess;
 import org.apache.spark.sql.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class DataCleaner {
 
     public static String getNullCount(SparkSession session, Dataset<Row> df){
         StringBuilder builder = new StringBuilder();
-        List<Column> cols = new ArrayList<>();
+        List<Long> cols = new ArrayList<>();
+        df.createOrReplaceTempView("wuzzuf");
         for(String col : df.columns()) {
-            Column c = df.col(col);
-            Column nulls = functions.count(functions.when(functions.isnan(c), c)).alias(col);
-            cols.add(nulls);
+            long c =  session.sql("select "+col+" as "+col+" from wuzzuf where "+col+" is null").count();
+            cols.add(c);
         }
-        df.select(cols.get(0)).show();
-        for(int i =0; i <cols.size();i++) {
-            for (Iterator<String> it = df.select(cols.get(i)).toJSON().toLocalIterator(); it.hasNext(); ) {
-                String js = it.next();
-                builder.append("<br>").append(js).append("</br>");
-            }
+        int count = 0;
+        for (String col : df.columns()) {
+            builder.append("<br>").append("<b>").append(col).append(": </b>").append(cols.get(count++)).append("</br>");
         }
         builder.insert(0, "<h1>Number Of nulls in wuzzuf dataset:</h1>");
         return builder.toString();
+    }
+
+
+    public static Dataset<Row> dropNulls(SparkSession session, Dataset<Row> df){
+        return df.filter(row -> !row.anyNull());
     }
 
 

0ee8942eecef7c45f427b04b943af8daadb17e66
azizmousa <azizmousa1010@gmail.com>
2021-07-02 16:40:49 +0200

    print summary of data

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/MainController.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/MainController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
@@ -30,6 +33,7 @@ public class MainController {
         StringBuilder jobs = new StringBuilder();
         jobs.append("<table style=\"width:100%\">");
         jobs.append(" <tr>")
+                .append("<th>#</th>")
                 .append("<th>Title</th>")
                 .append("<th>Company</th>")
                 .append("<th>Location</th>")
@@ -39,9 +43,11 @@ public class MainController {
                 .append("<th>Country</th>")
                 .append("<th>Skills</th>")
                 .append("</tr>");
+        int i = 1;
         for (Iterator<Row> it = MainModel.getMainDataframe().toLocalIterator(); it.hasNext(); ) {
             Row row = it.next();
             jobs.append(" <tr>")
+                    .append("<th>").append(i).append("</th>")
                     .append("<th>").append(row.getString(0)).append("</th>")
                     .append("<th>").append(row.getString(1)).append("</th>")
                     .append("<th>").append(row.getString(2)).append("</th>")
@@ -51,9 +57,21 @@ public class MainController {
                     .append("<th>").append(row.getString(6)).append("</th>")
                     .append("<th>").append(row.getString(7)).append("</th>")
                     .append("</tr>");
+            i++;
         }
         jobs.append("</table>");
         return jobs.toString();
+    }
+
+    @RequestMapping("/summary")
+    public String displaySummary(){
+        StringBuilder builder = new StringBuilder();
+        for(Iterator<String> it = MainModel.getMainDataframe().summary().toJSON().toLocalIterator(); it.hasNext();){
+            String js = it.next();
+            System.out.println(js);
+            builder.append("<br>").append(js).append("</br>");
+        }
+        return builder.toString();
     }
 
 }

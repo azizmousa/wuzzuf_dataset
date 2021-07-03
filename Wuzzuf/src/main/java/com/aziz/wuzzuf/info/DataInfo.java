156997ab620f1e830bf37c3257d7dc9a01d84e04
azizmousa <azizmousa1010@gmail.com>
2021-07-03 11:34:40 +0200

    get most popular areas

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/info/DataInfo.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/info/DataInfo.java
@@ -8,14 +8,24 @@ public class DataInfo {
 
     public static Dataset<Row> getMostDemandCompany(SparkSession session, Dataset<Row> df){
         df.createOrReplaceTempView("wuzzuf");
-        String sql = "select Company, count(Title) as jobs from wuzzuf  group by Company order by jobs";
+        String sql = "select Company, count(Title) as jobs from wuzzuf  group by Company order by jobs desc";
         return session.sql(sql);
     }
 
     public static Dataset<Row> getMostPopularJobTitle(SparkSession session, Dataset<Row> df){
         df.createOrReplaceTempView("wuzzuf");
-        String sql = "select Title, count(Title) as jobs from wuzzuf  group by Title order by jobs desc";
+        String sql = "select Title, count(Title) as titleCount from wuzzuf  group by Title order by titleCount desc";
         return session.sql(sql);
+    }
+
+
+    public static Dataset<Row> getMostPopularAreas(SparkSession session, Dataset<Row> df){
+        df.createOrReplaceTempView("wuzzuf");
+        String sql = "select Location, count(Location) as locCount from wuzzuf  " +
+                "group by Location order by locCount desc";
+        Dataset<Row> dataset = session.sql(sql);
+        dataset.show();
+        return dataset;
     }
 
 }

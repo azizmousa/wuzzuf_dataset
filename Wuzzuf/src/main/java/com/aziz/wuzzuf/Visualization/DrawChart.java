d7aec01a4c9e7397071b36d25ec6f53b76f3505e
azizmousa <azizmousa1010@gmail.com>
2021-07-04 22:43:33 +0200

    return bytecode of image instead of the path

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
@@ -1,9 +1,9 @@
 package com.aziz.wuzzuf.Visualization;
 
+import org.apache.commons.net.util.Base64;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.knowm.xchart.*;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,27 +11,24 @@ import java.util.List;
 public class DrawChart {
 
     public static String drawPieChart(Dataset<Row> dataset){
-        PieChart chart = new PieChartBuilder().width(800).height(600).title("jobs for each company").build();
+        PieChart chart = new PieChartBuilder().width(1000).height(600).title("jobs for each company").build();
         List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(10).collectAsList();
         for (Row row : rows){
            chart.addSeries(row.getString(0), row.getLong(1));
         }
-        String imageNameAndPath = "/images/jobs_PieChart.png";
+        byte[] imageBytes = new byte[]{};
         try {
-            BitmapEncoder.saveBitmap(chart, "./src/main/resources/static"+imageNameAndPath,BitmapEncoder.BitmapFormat.PNG);
-            System.out.println("./src/main/resources/static"+imageNameAndPath);
+            imageBytes = BitmapEncoder.getBitmapBytes(chart, BitmapEncoder.BitmapFormat.JPG);
         } catch (IOException e) {
             e.printStackTrace(); // Or something more intellegent
         }
-        return imageNameAndPath;
+        return Base64.encodeBase64String(imageBytes);
     }
 
     public static String drawBarChart(Dataset<Row> dataset){
 
-        CategoryChart chart = new CategoryChartBuilder().width(800).height(600).title("Popular Areas Histogram").xAxisTitle("Popular Areas").yAxisTitle("Count").build();
-        // Customize Chart
-        //chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
-        List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(10).collectAsList();
+        CategoryChart chart = new CategoryChartBuilder().width(1000).height(600).title("Popular Areas Histogram").xAxisTitle("Popular Areas").yAxisTitle("Count").build();
+        List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(5).collectAsList();
         List<String> keys = new ArrayList<>();
         List<Long> values = new ArrayList<>();
         for (Row row : rows){
@@ -40,13 +37,14 @@ public class DrawChart {
         }
         chart.getStyler().setHasAnnotations(true);
         chart.addSeries("Areas",keys,values);
-        String imageNameAndPath = "/images/areas_BarChart.png";
+        String imageNameAndPath = "barChart";
+        byte[] imageBytes = new byte[]{};
         try {
-            BitmapEncoder.saveBitmap(chart,"./src/main/resources/static"+imageNameAndPath,BitmapEncoder.BitmapFormat.PNG);
+            imageBytes = BitmapEncoder.getBitmapBytes(chart, BitmapEncoder.BitmapFormat.JPG);
         } catch (IOException e) {
             e.printStackTrace(); // Or something more intellegent
         }
-        return imageNameAndPath;
+        return Base64.encodeBase64String(imageBytes);
     }
 
 }

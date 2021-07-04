432d383db0846f815e50c1c50cc5ed22b35af2a2
azizmousa <azizmousa1010@gmail.com>
2021-07-04 23:10:29 +0200

    generalize the graph legend

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 public class DrawChart {
 
-    public static String drawPieChart(Dataset<Row> dataset){
-        PieChart chart = new PieChartBuilder().width(1000).height(600).title("jobs for each company").build();
+    public static String drawPieChart(Dataset<Row> dataset, String title){
+        PieChart chart = new PieChartBuilder().width(1000).height(600).title(title).build();
         List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(10).collectAsList();
         for (Row row : rows){
            chart.addSeries(row.getString(0), row.getLong(1));
@@ -25,9 +25,11 @@ public class DrawChart {
         return Base64.encodeBase64String(imageBytes);
     }
 
-    public static String drawBarChart(Dataset<Row> dataset){
+    public static String drawBarChart(Dataset<Row> dataset, String title){
 
-        CategoryChart chart = new CategoryChartBuilder().width(1000).height(600).title("Popular Areas Histogram").xAxisTitle("Popular Areas").yAxisTitle("Count").build();
+        CategoryChart chart = new CategoryChartBuilder().width(1000).height(600).title(title)
+                .xAxisTitle(dataset.columns()[0])
+                .yAxisTitle(dataset.columns()[1]).build();
         List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(5).collectAsList();
         List<String> keys = new ArrayList<>();
         List<Long> values = new ArrayList<>();
@@ -36,8 +38,7 @@ public class DrawChart {
             values.add(row.getLong(1));
         }
         chart.getStyler().setHasAnnotations(true);
-        chart.addSeries("Areas",keys,values);
-        String imageNameAndPath = "barChart";
+        chart.addSeries(dataset.columns()[0],keys,values);
         byte[] imageBytes = new byte[]{};
         try {
             imageBytes = BitmapEncoder.getBitmapBytes(chart, BitmapEncoder.BitmapFormat.JPG);

e919ba9664144be5fc7cb0f9196752ccce3c222e
Omar Wael <omar_wael1890@yahoo.com>
2021-07-04 18:11:52 +0200

    confirmation of pie and bar chart

--- Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
+++ Wuzzuf/src/main/java/com/aziz/wuzzuf/Visualization/DrawChart.java
@@ -5,16 +5,16 @@ import org.apache.spark.sql.Row;
 import org.knowm.xchart.*;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrawChart {
 
     public static String drawPieChart(Dataset<Row> dataset){
         PieChart chart = new PieChartBuilder().width(800).height(600).title("jobs for each company").build();
-        for (Iterator<Row> it = dataset.toLocalIterator(); it.hasNext(); ) {
-            Row row = it.next();
-            chart.addSeries(row.getString(0), Integer.parseInt(row.getString(1)));
+        List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(10).collectAsList();
+        for (Row row : rows){
+           chart.addSeries(row.getString(0), row.getLong(1));
         }
         String imageNameAndPath = "/images/jobs_PieChart.png";
         try {
@@ -31,16 +31,15 @@ public class DrawChart {
         CategoryChart chart = new CategoryChartBuilder().width(800).height(600).title("Popular Areas Histogram").xAxisTitle("Popular Areas").yAxisTitle("Count").build();
         // Customize Chart
         //chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
-        chart.getStyler().setHasAnnotations(true);
-
-        // Series
-        //todo real data
-        for (Iterator<Row> it = dataset.toLocalIterator(); it.hasNext(); ) {
-            Row row = it.next();
-            chart.addSeries(row.getString(0),Arrays.asList(new Integer[] { 0, 1, 2, 3, 4 }), Arrays.asList(new Integer[] { 4, 5, 9, 6, 5 }));
-
+        List<Row> rows = dataset.select(dataset.columns()[0],dataset.columns()[1]).limit(10).collectAsList();
+        List<String> keys = new ArrayList<>();
+        List<Long> values = new ArrayList<>();
+        for (Row row : rows){
+            keys.add(row.getString(0));
+            values.add(row.getLong(1));
         }
-
+        chart.getStyler().setHasAnnotations(true);
+        chart.addSeries("Areas",keys,values);
         String imageNameAndPath = "/images/areas_BarChart.png";
         try {
             BitmapEncoder.saveBitmap(chart,"./src/main/resources/static"+imageNameAndPath,BitmapEncoder.BitmapFormat.PNG);
